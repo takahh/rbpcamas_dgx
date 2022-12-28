2f4887257e50ba81b1e71f7adac0de4c8b49c49f
kimura.t <d@wakou.cc>
2022-12-28 20:11:24 +0900

    1228

--- find_why_loss_doesnot_go_down/compare_two_dataset.py
+++ find_why_loss_doesnot_go_down/compare_two_dataset.py
@@ -10,17 +10,36 @@ import numpy as np
 # constant
 # -------------------------------------------------------------------
 good_data_path = "/home/kimura.t/rbpcamas/data/Protein_centric/0.npz"
-bad_data_path = "/home/kimura.t/rbpcamas/data/ mydata_nored_10_10_f_0.01_F_0.8/0.npz"
+bad_data_path = "/home/kimura.t/rbpcamas/data/mydata_nored_10_10_f_0.01_F_0.8/0.npz"
+
+# -------------------------------------------------------------------
+# function
+# -------------------------------------------------------------------
+
+protein_cv_list_2805 = [['EIF3D', 'HNRNPL', 'BCLAF1', 'CPEB4', 'NSUN2', 'SMNDC1', 'AKAP8L', 'BCCIP', 'EIF3G', 'DDX24', 'METAP2', 'EIF4G2', 'LSM11', 'UCHL5', 'UTP18', 'AKAP1', 'SND1', 'DDX42', 'IGF2BP3', 'PTBP1', 'RBFOX2', 'GPKOW', 'SRSF7', 'STAU2', 'TAF15', 'SAFB2', 'ZC3H8', 'SF3B4', 'HNRNPA1', 'XRN2', 'SUGP2'], ['XPO5', 'LIN28B', 'NONO', 'SDAD1', 'ZNF622', 'SFPQ', 'SF3B1', 'UTP3', 'CPSF6', 'LARP7', 'NIPBL', 'ZRANB2', 'KHSRP', 'WRN', 'HNRNPC', 'SRSF1', 'EIF3H', 'ILF3', 'PPIG', 'CSTF2T', 'LARP4', 'SSB', 'AGGF1', 'U2AF2', 'SF3A3', 'YWHAG', 'FMR1', 'U2AF1', 'NCBP2', 'DKC1', 'TRA2A'], ['SERBP1', 'ZC3H11A', 'FXR2', 'FXR1', 'SUPV3L1', 'FKBP4', 'RPS11', 'GRSF1', 'ZNF800', 'SUB1', 'PPIL4', 'FUBP3', 'WDR3', 'NOLC1', 'UPF1', 'HNRNPU', 'HNRNPUL1', 'DROSHA', 'DDX52', 'QKI', 'AUH', 'TROVE2', 'DDX51', 'ABCF1', 'TBRG4', 'DDX21', 'XRCC6', 'GTF2F1', 'DGCR8', 'CDC40', 'MATR3'], ['PHF6', 'POLR2G', 'NIP7', 'WDR43', 'DHX30', 'FUS', 'MTPAP', 'NPM1', 'HNRNPK', 'SRSF9', 'PABPC4', 'PRPF4', 'DDX3X', 'RPS3', 'AATF', 'SBDS', 'DDX59', 'RBM15', 'DDX6', 'PUM2', 'EXOSC5', 'HLTF', 'RBM27', 'PUM1', 'PUS1', 'GEMIN5', 'GRWD1', 'RBM5', 'CSTF2', 'FTO', 'SLTM'], ['GNL3', 'EWSR1', 'AARS', 'NKRF', 'FAM120A', 'PCBP2', 'FASTKD2', 'EFTUD2', 'PRPF8', 'RBM22', 'IGF2BP2', 'BUD13', 'SAFB', 'TIAL1', 'NOL12', 'IGF2BP1', 'PCBP1', 'SLBP', 'AQR', 'DDX55', 'APOBEC3C', 'G3BP1', 'KHDRBS1', 'PABPN1', 'TARDBP', 'HNRNPM', 'TIA1', 'YBX3', 'RPS5', 'TNRC6A']]
 
 # -------------------------------------------------------------------
 # function
 # -------------------------------------------------------------------
 
 
+def get_pro_id_dict():
+    id = 0
+    id_dict = {}
+    for item in protein_cv_list_2805:
+        for name in item:
+            id_dict[name] = id
+            if id == 50:
+                print(name)
+            id += 1
+
 def main():
-    arr = np.load(good_data_path, allow_pickle=True)
+    get_pro_id_dict()
+    arr = np.load(bad_data_path, allow_pickle=True)
     for files in arr.files:
-        print(f"{files}")
+        print(files)
+        print(arr[files].shape)
+        print(f"{arr[files][0]}")
 
 
 # -------------------------------------------------------------------
