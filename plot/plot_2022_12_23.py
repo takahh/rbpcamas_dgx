ebfbbfe16d82cbe84e368b5be5bfd94ff00dc6da
Takayuki Kimura <d@wakou.cc>
2023-01-03 08:13:00 +0900

    103

--- plot/plot_2022_12_23.py
+++ plot/plot_2022_12_23.py
@@ -21,47 +21,15 @@ figpath = f"/home/kimura.t/rbpcamas/python/Figures/protein_cent/"
 # -------------------------------------------------------------------
 
 
-def get_data_per_file(filename):
+def get_data_per_file2(filename):
     with open(filename) as f:
-        filenum = 200  # 2000/40 * 4
         allauclist, alllosslist = [], []
-        subauclist_all, sublosslist_all, pred_arr, label_arr = [], [], None, None
-        totalstep = 0
-        epochcount = 0
         print("working...")
         for lines in f.readlines():
-            if "ROC" in lines:
-                totalstep += 1
-                listt, sublist = [], []
-                line1 = lines.split(":")[2].split("]-")[0].strip("]").split("[")
-                list1 = [x.strip("'").strip(" ").strip("]").replace("  ", " ") for x in line1][2:]
-                list1 = [x[:-1] if x[-1] == " " else x for x in list1]
-                list1 = [x.split(" ") for x in list1]
-                for x in list1:
-                    for y in x:
-                        if len(y) > 0:
-                            sublist.append(y)
-                    listt.append(sublist)
-                    sublist = []
-                list1 = [[float(e) for e in x] for x in listt]
-                line2 = lines.split(":")[2].split("or(")[1]
-                list2 = line2.split(",")[0]
-                list2 = [x.strip("  [").split(" ") for x in list2.strip("[[[").split("]") if x]
-                list2 = [[int(x) for x in item] for item in list2]
-                # make two arrays for AUC caculation
-                if totalstep == 1:
-                    pred_arr = np.array(list1)
-                    label_arr = np.array(list2)
-                else:
-                    pred_arr = np.concatenate([pred_arr, np.array(list1)])
-                    label_arr = np.concatenate([label_arr, np.array(list2)])
-                # calc. AUC and save to lists, the end of epoch
-                if totalstep == filenum:
-                    allauclist.append(metrics.roc_auc_score(label_arr[:, 1], pred_arr[:, 1]))
-                    alllosslist.append(metrics.log_loss(label_arr, pred_arr))
-                    subauclist, sublosslist = [], []
-                    totalstep = 0
-                    epochcount += 1
+            if "loss" in lines and "step" in lines:
+                # 50/50 [==============================] - 164s 3s/step - loss: 0.6608 - auc: 0.6288
+                loss = lines.split("loss:")[1].split("auc")[0].strip()
+                print(f"###{loss}###")
         return alllosslist
 
 
